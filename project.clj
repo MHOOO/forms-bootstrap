ff9a17d8f4e2a61bee03434b3157d0ab19640c63
Thomas Karolski <Thomas.Karolski@googlemail.com>
2012-10-18 16:22:17 +0200

    - swap sandbar for validateur

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [noir "1.3.0-beta10"]
                  [paddleguru/enlive "1.2.0-alpha1"]
-                 [org.clojars.dpetrovics/sandbar "0.4.0-SNAPSHOT"]
+                 [com.novemberain/validateur "1.2.0"]
                  [ring/ring-core "1.1.1"]]
   :plugins [[lein-swank "1.4.4"]
             [lein-ring "0.7.4"]]
